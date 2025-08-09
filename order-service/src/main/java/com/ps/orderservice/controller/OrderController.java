1dcaa14ae088dbbd536b05de2d38dff2ccd69a6a
Hle1801 <hoang.lvforwork@gmail.com>
2025-08-09 22:52:40 +0700

    init project

---
+++ order-service/src/main/java/com/ps/orderservice/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.ps.orderservice.controller;
+
+import com.ps.orderservice.entity.Order;
+import com.ps.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderRepository orderRepository;
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderRepository.save(order);
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+}
---
+++ user-service/src/main/java/com/ps/userservice/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.ps.userservice.controller;
+
+import com.ps.userservice.entity.User;
+import com.ps.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+}
