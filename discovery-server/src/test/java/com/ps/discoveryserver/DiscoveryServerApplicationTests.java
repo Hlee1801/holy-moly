96e097a7a673990fd7594052c0e929787b06dee1
Hle1801 <hoang.lvforwork@gmail.com>
2025-08-11 20:55:54 +0700

    add discovery-server

---
+++ discovery-server/src/test/java/com/ps/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
